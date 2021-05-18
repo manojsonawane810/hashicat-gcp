abd94e5c5ec5a36bf3a4a1dd345ca82172a923d1
Manoj Sonawane <manojsonawane810@gmail.com>
2021-05-18 11:47:29 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/ManojSonawane-training/network/google"
+  version = "3.2.2"
+  # insert required variables here
+  network_name = "manoj-network"
+  project_id = "var.project"
+  subnets = [
+  {
+    subnet_name   = "manoj-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
